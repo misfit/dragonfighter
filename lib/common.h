cc56ea6d91fbb93de5c6c0756c75dd165b76ac17
Brian S. Moriya <brian.moriya@gmail.com>
2009-09-10 09:24:53 -0700

    setup_game, teardown_game and load map are being worked on.

--- lib/common.h
+++ lib/common.h
@@ -22,183 +22,22 @@
 #define _COMMON_H
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <allegro.h>
 
-#define GAMENAME "Dragon Fighter"
-#define MODE GFX_AUTODETECT_WINDOWED
-#define WIDTH 512
-#define HEIGHT 480
-#define BLACK makecol(0,0,0)
-#define WHITE makecol(255,255,255)
-#define UP 0
-#define LEFT 1
-#define DOWN 2
-#define RIGHT 3
+#define MODE GFX_AUTODETECT_WINDOWED /* for set_gfx_mode call */
+#define WIDTH 768 /* total width of the game window */
+#define HEIGHT 720 /* total height of the game window */
 
-/**** For map drawing ****/
-#define TILEW 32
-#define TILEH 32
-#define COLS 10
-#define MAP_ACROSS 24
-#define MAP_DOWN 24
-#define MAPW MAP_ACROSS*TILEW
-#define MAPH MAP_DOWN*TILEH
+/**** constants relating to the player character ****/
+#define MAXSPEED 4 /* Normal speed the player walks at */
+#define HILLSPEED 2 /* Speed over rough terrain */
 
-#define THRONE_ROOM 0
-#define TANTAGEL_CASTLE_CYARD 1
-#define TANTAGEL_CASTLE_BASEMT 2
-#define TR0 0
-
-#define GAMEOVER 0
-#define ROAMING 1
-#define TEST 2
-
-/**** Global structs and variables ****/
-
-typedef struct {
-  struct NODE *next;
-}NODE;
-
-/*
- * Struct for the hero of the game. 
- */
-typedef struct {
-  /* Motion variables */
-  int x, y;
-  int width, height;
-  int xspeed, yspeed;
-  int xdelay, ydelay;
-  int xcount, ycount;
-  int currentframe;
-  int maxframe;
-  int animdirection;
-  int framecount;
-  int framedelay;
-  int facing;
-  /* Attributes variables */
-  int currentlocation;
-}HERO;
-
-/*
- * Struct for tiles int the map.
- */
-typedef struct {
-  int left;
-  int top;
-  int right;
-  int bottom;
-  int width, height;
-}BLOCK;
-
-typedef struct {
-  BLOCK *block;
-  struct BLOCKNODE *next;
-}BLOCKNODE;
-
-typedef struct {
-  int type;
-  int id;
-  BLOCK *block;
-  struct NOWALKNODE *next;
-}NOWALKNODE;
-
-typedef struct {
-  int map;
-}MAP;
-
-typedef struct {
-  NOWALKNODE *nowalkshead;
-}PLACE;
-
-/**** Character bitmaps and sprites ****/
-
-HERO *hero;
-BITMAP *hero_left_images[2];
-BITMAP *hero_right_images[2];
-BITMAP *hero_up_images[2];
-BITMAP *hero_down_images[2];
-BITMAP *tempbitmap;
-BITMAP *scroll;
-BITMAP *buffer;
-BITMAP *tiles;
-
-/* PLACE definitions
- * format is:
- * l = locked
- * u = unlocked
- * # = door number
- * full = [[l/u/][#]]...[name]
- */
-PLACE *l0throneroom;
-PLACE *u0throneroom;
-PLACE *l1l2tantagel_courtyard;
-PLACE *u1l2tantagel_courtyard;
-PLACE *l1u2tantagel_courtyard;
-PLACE *u1u2tantagel_courtyard;
-
-int n; 
-
-/* Position variables for laying tiles */
-int tilex, tiley;
-int scrollx, scrolly;
-int gamestate;
-
-/**** Function prototypes ****/
-
-void initialize_game (int colordepth);
-
-void setup_hero (void);
-
-void cleanup_hero (void);
-
-void cleanup_locked_thronerooom (void);
-
-void cleanup_unlocked_throneroom (void);
-
-void cleanup_l1l2tantagel_castle (void);
-
-BITMAP *grab_frame (BITMAP *source, int width, int height,
-		   int startx, int starty, int columns, int frame);
-
-void draw_frame (BITMAP *source, BITMAP *dest, int x, int y, int width,
-		int height, int startx, int starty, int columns, int frame);
-
-void cleanup (void);
-
-int is_inside (int x, int y, int left, int top, int right, int bottom);
-
-unsigned char get_input (PLACE *place);
-
-void move_hero_up (void);
-
-void move_hero_down (void);
-
-void move_hero_right (void);
-
-void move_hero_left (void);
-
-void animate_hero (void);
-
-void draw_hero (void);
-
-void move_hero (void);
-
-int check_collision (PLACE *place);
-
-void add_nowalk (PLACE *place, NOWALKNODE *newnode);
-
-BLOCK *create_new_block (void);
-
-void draw_locked_throneroom (void);
-
-void draw_unlocked_throneroom (void);
-
-void draw_tantagel_courtyard (void);
-
-PLACE *load_map (const unsigned char curlocation);
-
-#define EXIT 200
+/**** map constants ****/
+#define TILEW 32 /* Width of the tiles used for the map */
+#define TILEH 32 /* Height of the tiles used for the map */
+#define TILES 13 /* How many tiles are on the spritesheet */
+#define COLS 10  /* How many columns of tiles are on the spritesheet */
+/* Correspond to position on the spritesheet and in the map array */
 #define COUNTER 0
 #define BRICK 1
 #define STONE 2
@@ -212,121 +51,113 @@ PLACE *load_map (const unsigned char curlocation);
 #define STAIRSDOWNL 11
 #define STAIRSUPR 12
 #define STAIRSUPL 13
+/* specific maps */
+#define TTRL0 "tantthronerooml0.fmp"  /* First map in the game */
+#define TTRL0_ACROSS 40 /* Tiles across the first map spans */
+#define TTRL0_DOWN 40   /* Tiles down the first map spans */
+#define TTRL0W TTRL0_ACROSS*TILEW /* Pixels across the first map spans */
+#define TTRL0H TTRL0_DOWN*TILEH   /* Pixels down the first map spans */
 
-/* Maps
+/**** game state constants ****/
+#define GAMEOVER 0
+#define ROAMING 1
+#define TEST 2
+
+/**** Global structs and variables ****/
+
+/* player character */
+typedef struct {
+  int direction;
+  int x, y;
+  int width, height;
+  int speed;
+  int xdelay, ydelay;
+  int xcount, ycount;
+  int currentframe;
+  int maxframe;
+  int framecount;
+  int framedelay;
+}PLAYER;
+PLAYER *hero;
+
+/* Struct for tiles in the map. */
+typedef struct {
+  int left;
+  int top;
+  int right;
+  int bottom;
+  int width, height;
+}BLOCK;
+
+/**** bitmaps ****/
+BITMAP *hero_left_bmps[2];
+BITMAP *hero_right_bmps[2];
+BITMAP *hero_up_bmps[2];
+BITMAP *hero_down_bmps[2];
+BITMAP *scroll; /* virtual background buffer */
+BITMAP *buffer; /* double buffer */
+
+/* PLACE definitions
  * format is:
- * # = door numbers of unlocked doors on map.
- * name = variable name
- * full = [name][#]...
+ * l = locked
+ * u = unlocked
+ * # = door number
+ * full = [[l/u/][#]]...[name]
+ 
+PLACE *l0throneroom;
+PLACE *u0throneroom;
+PLACE *l1l2tantagel_courtyard;
+PLACE *u1l2tantagel_courtyard;
+PLACE *l1u2tantagel_courtyard;
+PLACE *u1u2tantagel_courtyard;
+*/
+
+/**** global variables ****/
+int tilex, tiley;
+int scrollx, scrolly;
+int startx, starty;
+int gamestate;
+
+/**** Function prototypes ****/
+void setup_game (void);
+/*
+ * Does necessary initializations for starting the game.
  */
-#define LTR 0
-#define LTRACROSS 24
-#define LTRDOWN 24
-#define DR1 1
-int throneroommap0[] = {
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,0,0,0,0,0,0,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,0,1,0,0,1,0,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,10,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,11,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
 
-};
+void setup_player (void);
+/*
+ * Performs necessary initializations for setting up the player.
+ */
 
-#define UTR 1
-#define UTRACROSS 24
-#define UTRDOWN 24
-#define SD1 1
-int throneroommap[] = {
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,0,0,0,0,0,0,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,0,1,0,0,1,0,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,1,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,1,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,1,1,1,1,1,1,1,11,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-};
+void teardown_player (void);
+/*
+ * Tearsdown player.
+ */
 
-#define TCY 2
-#define TCYACROSS 32
-#define TCYDOWN 34
-#define DR2 2
-#define DR3 3
-#define SD2 2
-#define SU1 1
-int tantagelcourtyard12[] = {
-  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-  3,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,3,2,2,2,3,7,3,3,3,
-  3,2,1,1,1,1,1,2,3,3,3,3,3,3,3,3,2,1,1,1,1,1,2,3,2,1,2,3,3,3,3,3,
-  3,2,1,1,1,1,1,2,3,3,3,3,3,3,3,3,2,1,1,1,1,1,2,3,2,0,2,3,3,3,3,3,
-  3,2,1,1,2,1,1,2,2,2,2,1,1,2,2,2,2,1,1,2,1,1,2,3,3,3,7,7,3,3,3,3,
-  3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,3,7,7,7,3,3,3,3,3,
-  3,2,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,2,3,3,3,3,3,3,3,3,3,
-  3,2,2,2,2,2,1,2,1,1,1,1,1,1,1,1,2,2,2,10,2,2,2,2,2,1,2,2,2,3,3,3,
-  3,2,1,1,1,2,1,2,13,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,2,3,3,3,
-  3,2,1,1,1,1,1,2,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,3,3,3,
-  3,2,1,1,1,2,1,2,2,2,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,3,3,3,
-  3,2,2,2,2,2,1,2,7,7,1,1,1,1,7,7,2,1,1,2,1,1,2,1,1,2,1,1,2,3,3,3,
-  3,2,1,1,1,2,1,2,7,7,1,1,1,1,7,7,2,1,1,2,1,1,2,1,1,2,1,1,2,3,3,3,
-  3,2,1,1,1,2,1,2,7,3,1,1,1,1,3,7,2,1,1,1,1,1,1,1,1,1,1,1,2,3,3,3,
-  3,2,9,1,1,10,1,2,3,3,1,1,1,1,3,3,2,1,1,1,1,1,1,1,1,1,1,1,2,3,3,3,
-  3,2,1,9,1,2,1,2,3,3,1,1,1,1,3,3,2,1,1,2,1,1,2,1,1,2,1,1,2,3,3,3,
-  3,2,9,1,9,2,1,2,3,1,1,1,1,1,1,3,2,1,1,2,1,1,2,1,1,2,1,1,2,3,3,3,
-  3,2,2,2,2,2,1,2,3,1,6,6,6,6,1,3,2,2,2,2,2,2,2,2,2,2,1,2,2,3,3,3,
-  3,2,1,1,1,1,1,1,1,1,6,5,5,6,1,1,1,1,1,1,1,1,2,1,1,1,1,1,2,3,3,3,
-  3,2,1,1,1,1,1,1,1,1,6,5,5,6,1,1,1,1,1,1,1,1,2,5,5,5,5,5,2,3,3,3,
-  3,2,2,2,1,1,2,2,2,1,6,6,6,6,1,2,2,1,1,1,1,1,2,5,5,5,5,5,2,3,3,3,
-  3,2,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,1,1,1,1,2,3,3,3,
-  3,2,1,1,1,1,1,1,2,2,1,1,1,1,2,2,2,2,2,2,1,1,2,1,1,1,1,1,2,6,3,3,
-  3,2,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,1,2,2,2,2,2,2,2,6,3,3,
-  3,2,1,1,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,1,2,6,6,6,6,6,6,6,3,3,
-  3,2,1,6,6,1,1,2,1,2,1,1,1,1,2,1,1,2,2,2,2,2,2,6,6,6,6,6,6,6,3,3,
-  3,2,6,6,6,6,1,1,1,2,1,1,1,1,2,1,1,2,1,1,2,1,2,6,6,6,6,6,6,6,3,3,
-  3,2,6,6,6,6,1,1,1,2,2,1,1,2,2,1,1,1,1,1,0,1,2,6,6,6,6,6,6,6,3,3,
-  3,2,6,6,6,6,6,1,1,2,1,1,1,1,2,1,1,2,1,1,2,1,2,6,6,6,6,6,6,6,3,3,
-  3,2,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,2,2,2,2,2,2,6,6,6,6,6,6,6,3,3,
-  3,6,6,3,3,3,3,3,3,3,3,1,1,3,3,3,3,3,3,3,3,6,6,6,6,6,6,6,6,6,11,3,
-  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-};
+void teardown_game (void);
+/*
+ * Tearsdown the game.
+ */
 
-unsigned int mapw = LTRACROSS*TILEW;
-unsigned int maph = LTRDOWN*TILEH;
+BITMAP *grabframe (BITMAP *source, int width, int height,
+                  int startx, int starty, int columns, int frame);
+/*
+ * Extracts an image from a spritesheet.
+ */
+
+void draw_ttrl0 (void);
+/*
+ * Draws the throne room with the locked door.
+ */
+
+void load_map (char *map);
+/*
+ * Loads the given map.
+ */
+
+int main (void);
+/*
+ * Main function and game loop.
+ */
 
 #endif
