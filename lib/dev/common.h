06b787d274082440fe5e60662cc9aaa14b70afd9
Brian Moriya <brian.moriya@gmail.com>
2009-09-30 16:16:42 -0700

    Got first working version of mainloop and initializations.

--- lib/dev/common.h
+++ lib/dev/common.h
@@ -9,5 +9,46 @@
 
 #include <allegro.h>
 
+/* Allegro constants */
+#define MODE GFX_AUTODETECT_WINDOWED
+#define WIDTH 640
+#define HEIGHT 480
+
+/* scrolling constants */
+#define SCROLL_STEP 8
+
+/* map constants */
+#define TILEW 32
+#define TILEH 32
+#define COLS 10
+
+/* Tantagel castle courtyard */
+#define TCB_ACROSS 32
+#define TCB_DOWN 32
+#define TCBW TCB_ACROSS*TILEW
+#define TCBH TCB_DOWN*TILEH
+extern int TCBLALB[];
+
+/* bitmaps */
+BITMAP *bufferbmp;
+BITMAP *scrollbmp;
+BITMAP *tilesbmp;
+
+/* position variables */
+int scrollx;
+int scrolly;
+int tilex, tiley;
+
+/* function prototypes */
+
+void setup_allegro (int mode, int screenw, int screenh, int cdepth);
+void setup_bmps (void);
+void destroy_bmps (void);
+void draw_frame (BITMAP *source, BITMAP *destination, int x, int y, int width, 
+		int height, int startx, int starty, int columns, int frame);
+/*
+ * Taken from Game Programming All in One 3rd ed.
+ */
+void draw_TCBLALB (void);
 
 #endif
--- lib/dev/main.c
+++ lib/dev/main.c
@@ -7,6 +7,63 @@
 #include "common.h"
 
 int main (void) {
+  /* initialize the game. */
+  setup_allegro (MODE, WIDTH, HEIGHT, 16);
+  setup_bmps();
+  scrollx = 0;
+  scrolly = 0;
+
+  while (!key[KEY_ESC]) {
+    blit (scrollbmp, bufferbmp, scrollx, scrolly, 0, 0, WIDTH-1, HEIGHT-1);
+    
+    acquire_screen();
+    blit (bufferbmp, screen, 0, 0, 0, 0, WIDTH-1, HEIGHT-1);
+    release_screen();
+    
+    rest (20);
+  }
+  destroy_bmps();
+  allegro_exit();
   return 0;
 }
 END_OF_MAIN()
+
+void setup_allegro (int mode, int screenw, int screenh, int cdepth) {
+  allegro_init();
+  install_keyboard();
+  install_timer();
+  srand (time (NULL));
+  set_color_depth (cdepth);
+  set_gfx_mode (mode, screenw, screenh, 0, 0);
+}
+
+void setup_bmps (void) {
+  bufferbmp = create_bitmap (WIDTH, HEIGHT);
+  scrollbmp = create_bitmap (TCBW, TCBH);
+  tilesbmp = load_bitmap ("maptiles.bmp", NULL);
+}
+
+void destroy_bmps (void) {
+  destroy_bitmap (bufferbmp);
+  destroy_bitmap (scrollbmp);
+  destroy_bitmap (tilesbmp);
+}
+
+void draw_frame (BITMAP *source, BITMAP *destination, int x, int y, int width,
+                int height, int startx, int starty, int columns, int frame) {
+  /* calculate frame position */
+  int framex = startx + (frame%columns)*width;
+  int framey = starty + (frame/columns)*height;
+  /* draw frame to destination bitmap. */
+  masked_blit (source, destination, framex, framey, x, y, width, height);
+}
+
+void draw_TCBLALB (void) {
+  int i = 0;
+  for (tiley = 0; tiley < scrollbmp->h; tiley += TILEH) {
+    for (tilex = 0; tilex < scrollbmp->w; tilex += TILEW) {
+      draw_frame (tilesbmp, scrollbmp, tilex, tiley, TILEW, TILEH, 0, 0,
+                  COLS, TCBLALB[i++]);
+    }
+  }
+}
