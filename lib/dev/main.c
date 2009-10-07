1238f3075096b583ed81bfd96f22ede5f3f1cc25
Brian Moriya <brian.moriya@gmail.com>
2009-10-07 14:17:58 -0700

    Moved input to its own implementation file.

--- lib/dev/main.c
+++ lib/dev/main.c
@@ -48,49 +48,6 @@ int main (void) {
 END_OF_MAIN()
 
 
-void get_input (void) {
-  if (key[KEY_RIGHT]) {
-    player->direction = RIGHT;
-    if (currentmap->idnumber == ALEFGARD1 || 
-	currentmap->idnumber == ALEFGARD2){
-      player->xspeed = PLAINS_SPEED;
-      player->framedelay = 5;
-    }
-    else player->xspeed = NORM_SPEED;
-    
-  } else if (key[KEY_LEFT]) {
-    player->direction = LEFT;
-    if (currentmap->idnumber == ALEFGARD1 || 
-	currentmap->idnumber == ALEFGARD2) {
-      player->xspeed = -PLAINS_SPEED;
-      player->framedelay = 5;
-    }
-    else player->xspeed = -NORM_SPEED;
-    
-  } else if (key[KEY_DOWN]) {
-    player->direction = DOWN;
-    if (currentmap->idnumber == ALEFGARD1 || 
-	currentmap->idnumber == ALEFGARD2) {
-      player->yspeed = PLAINS_SPEED;
-      player->framedelay = 5;
-    }
-    else player->yspeed = NORM_SPEED;
-    
-  } else if (key[KEY_UP]) {
-    player->direction = UP;
-    if (currentmap->idnumber == ALEFGARD1 || 
-	currentmap->idnumber == ALEFGARD2) {
-      player->yspeed = -PLAINS_SPEED;
-      player->framedelay = 5;
-    }
-    else player->yspeed = -NORM_SPEED;
-    
-  } else {
-    player->xspeed = 0;
-    player->yspeed = 0;
-  }
-}
-
 void draw_map (int map[]) {
   int i = 0;
 
