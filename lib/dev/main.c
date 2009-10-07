632af7a9080af7c73c4e5c11cd39cfc0325bb981
Brian Moriya <brian.moriya@gmail.com>
2009-10-07 14:12:22 -0700

    Got the setup functions into their own file.

--- lib/dev/main.c
+++ lib/dev/main.c
@@ -47,38 +47,6 @@ int main (void) {
 }
 END_OF_MAIN()
 
-void setup_allegro (int mode, int screenw, int screenh, int cdepth) {
-  allegro_init();
-  install_keyboard();
-  install_timer();
-  srand (time (NULL));
-  set_color_depth (cdepth);
-  set_gfx_mode (mode, screenw, screenh, 0, 0);
-}
-
-void setup_bmps (void) {
-  int i;
-  BITMAP *tempbmp;
-
-  bufferbmp = create_bitmap (WIDTH, HEIGHT);
-  tilesbmp = load_bitmap ("maptiles.bmp", NULL);
-
-  /* load bitmaps for the player animations */
-  tempbmp = load_bitmap ("chars.bmp", NULL);
-  for (i = 0; i < 2; i++) {
-    playerdnbmp[i] = grab_frame (tempbmp, 32, 32, 0, 0, 32, i);
-    playerupbmp[i] = grab_frame (tempbmp, 32, 32, 0, 0, 32, i+2);
-    playerrtbmp[i] = grab_frame (tempbmp, 32, 32, 0, 0, 32, i+4);
-    playerltbmp[i] = grab_frame (tempbmp, 32, 32, 0, 0, 32, i+6);
-  }
-  destroy_bitmap (tempbmp);
-}
-
-void destroy_bmps (void) {
-  destroy_bitmap (bufferbmp);
-  destroy_bitmap (scrollbmp);
-  destroy_bitmap (tilesbmp);
-}
 
 void draw_frame (BITMAP *source, BITMAP *destination, int x, int y, int width,
                 int height, int startx, int starty, int columns, int frame) {
@@ -409,15 +377,19 @@ void map_handler (void) {
     break;
 
   case ALEFGARD1:
-    /* player coming from Tantagel Castle */
-    if (currentmap->initflag == 1) {
-      scrollbmp = create_bitmap (ALEFGARDW, ALEFGARDH);
-      clear (scrollbmp);
-      player->x = ALEFGARDTCstartx;
-      player->y = ALEFGARDTCstarty;
-      scrollx = 1300;
-      scrolly = 1300;
-      currentmap->initflag = 0;
+    switch (currentmap->pointofentry) {
+    case 0:
+      /* player coming from Tantagel Castle */
+      if (currentmap->initflag == 1) {
+	scrollbmp = create_bitmap (ALEFGARDW, ALEFGARDH);
+	clear (scrollbmp);
+	player->x = ALEFGARDTCstartx;
+	player->y = ALEFGARDTCstarty;
+	scrollx = 1300;
+	scrolly = 1300;
+	currentmap->initflag = 0;
+      }
+      break;
     }
     draw_map (ALEFGARD);
     break;
@@ -426,15 +398,19 @@ void map_handler (void) {
     break;
 
   case BYA:
-    /* player entered through the gate */
-    if (currentmap->initflag == 1) {
-      scrollbmp = create_bitmap (BYAW, BYAH);
-      clear (scrollbmp);
-      player->x = BYA1startx;
-      player->y = BYA1starty;
-      scrollx = 0;
-      scrolly = 128;
-      currentmap->initflag = 0;
+    switch (currentmap->pointofentry) {
+    case 0:
+      /* player entered through the gate */
+      if (currentmap->initflag == 1) {
+	scrollbmp = create_bitmap (BYAW, BYAH);
+	clear (scrollbmp);
+	player->x = BYA1startx;
+	player->y = BYA1starty;
+	scrollx = 0;
+	scrolly = 128;
+	currentmap->initflag = 0;
+      }
+      break;
     }
     draw_map (BRECONARYL3L4);
     break;
